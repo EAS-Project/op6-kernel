2b9dcb89d5788f3e7f73b9c1f372f5be0b55c5b4
Anurag Chouhan <achouhan@codeaurora.org>
2019-02-08 15:46:53 +0530

    icnss: Add Api to Block/Unblock modem shutdown
    Add API to Block/Unblock modem graceful shutdown.
    
    Change-Id: I69b061fc7d25762b2c36d9590802addfc170f91f
    Signed-off-by: Anurag Chouhan <achouhan@codeaurora.org>

--- drivers/soc/qcom/icnss.c
+++ drivers/soc/qcom/icnss.c
@@ -291,7 +291,7 @@ enum icnss_driver_state {
 	ICNSS_DRIVER_UNLOADING,
 	ICNSS_REJUVENATE,
 	ICNSS_MODE_ON,
-	ICNSS_DRIVER_LOADING,
+	ICNSS_BLOCK_SHUTDOWN,
 };
 
 struct ce_irq_list {
@@ -481,7 +481,7 @@ static struct icnss_priv {
 	struct mutex dev_lock;
 	uint32_t fw_error_fatal_irq;
 	uint32_t fw_early_crash_irq;
-	struct completion driver_probed;
+	struct completion unblock_shutdown;
 } *penv;
 
 #ifdef CONFIG_ICNSS_DEBUG
@@ -1170,6 +1170,21 @@ bool icnss_is_fw_ready(void)
 		return test_bit(ICNSS_FW_READY, &penv->state);
 }
 EXPORT_SYMBOL(icnss_is_fw_ready);
+
+void icnss_block_shutdown(bool status)
+{
+	if (!penv)
+		return;
+
+	if (status) {
+		set_bit(ICNSS_BLOCK_SHUTDOWN, &penv->state);
+		reinit_completion(&penv->unblock_shutdown);
+	} else {
+		clear_bit(ICNSS_BLOCK_SHUTDOWN, &penv->state);
+		complete(&penv->unblock_shutdown);
+	}
+}
+EXPORT_SYMBOL(icnss_block_shutdown);
 
 bool icnss_is_fw_down(void)
 {
@@ -2287,8 +2302,7 @@ static int icnss_call_driver_probe(struct icnss_priv *priv)
 
 	icnss_hw_power_on(priv);
 
-	set_bit(ICNSS_DRIVER_LOADING, &priv->state);
-	reinit_completion(&penv->driver_probed);
+	icnss_block_shutdown(true);
 	while (probe_cnt < ICNSS_MAX_PROBE_CNT) {
 		ret = priv->ops->probe(&priv->pdev->dev);
 		probe_cnt++;
@@ -2298,13 +2312,11 @@ static int icnss_call_driver_probe(struct icnss_priv *priv)
 	if (ret < 0) {
 		icnss_pr_err("Driver probe failed: %d, state: 0x%lx, probe_cnt: %d\n",
 			     ret, priv->state, probe_cnt);
-		complete(&penv->driver_probed);
-		clear_bit(ICNSS_DRIVER_LOADING, &penv->state);
+		icnss_block_shutdown(false);
 		goto out;
 	}
 
-	complete(&penv->driver_probed);
-	clear_bit(ICNSS_DRIVER_LOADING, &penv->state);
+	icnss_block_shutdown(false);
 	set_bit(ICNSS_DRIVER_PROBED, &priv->state);
 
 	return 0;
@@ -2443,8 +2455,7 @@ static int icnss_driver_event_register_driver(void *data)
 	if (ret)
 		goto out;
 
-	set_bit(ICNSS_DRIVER_LOADING, &penv->state);
-	reinit_completion(&penv->driver_probed);
+	icnss_block_shutdown(true);
 	while (probe_cnt < ICNSS_MAX_PROBE_CNT) {
 		ret = penv->ops->probe(&penv->pdev->dev);
 		probe_cnt++;
@@ -2454,13 +2465,11 @@ static int icnss_driver_event_register_driver(void *data)
 	if (ret) {
 		icnss_pr_err("Driver probe failed: %d, state: 0x%lx, probe_cnt: %d\n",
 			     ret, penv->state, probe_cnt);
-		clear_bit(ICNSS_DRIVER_LOADING, &penv->state);
-		complete(&penv->driver_probed);
+		icnss_block_shutdown(false);
 		goto power_off;
 	}
 
-	complete(&penv->driver_probed);
-	clear_bit(ICNSS_DRIVER_LOADING, &penv->state);
+	icnss_block_shutdown(false);
 	set_bit(ICNSS_DRIVER_PROBED, &penv->state);
 
 	return 0;
@@ -2729,8 +2738,8 @@ static int icnss_modem_notifier_nb(struct notifier_block *nb,
 		return NOTIFY_OK;
 
 	if (code == SUBSYS_BEFORE_SHUTDOWN && !notif->crashed &&
-	    test_bit(ICNSS_DRIVER_LOADING, &priv->state)) {
-		if (!wait_for_completion_timeout(&priv->driver_probed,
+	    test_bit(ICNSS_BLOCK_SHUTDOWN, &priv->state)) {
+		if (!wait_for_completion_timeout(&priv->unblock_shutdown,
 						 PROBE_TIMEOUT))
 			icnss_pr_err("wlan driver probe timeout\n");
 	}
@@ -4101,8 +4110,8 @@ static int icnss_stats_show_state(struct seq_file *s, struct icnss_priv *priv)
 		case ICNSS_MODE_ON:
 			seq_puts(s, "MODE ON DONE");
 			continue;
-		case ICNSS_DRIVER_LOADING:
-			seq_puts(s, "WLAN DRIVER LOADING");
+		case ICNSS_BLOCK_SHUTDOWN:
+			seq_puts(s, "BLOCK SHUTDOWN");
 		}
 
 		seq_printf(s, "UNKNOWN-%d", i);
@@ -4777,7 +4786,7 @@ static int icnss_probe(struct platform_device *pdev)
 		 &dev_attr_cnss_version_information);
 	push_component_info(WCN, "WCN3990", "QualComm");
 
-	init_completion(&priv->driver_probed);
+	init_completion(&priv->unblock_shutdown);
 
 	icnss_pr_info("Platform driver probed successfully\n");
 
@@ -4803,7 +4812,7 @@ static int icnss_remove(struct platform_device *pdev)
 	device_remove_file(&penv->pdev->dev,
 		 &dev_attr_cnss_version_information);
 
-	complete_all(&penv->driver_probed);
+	complete_all(&penv->unblock_shutdown);
 
 	icnss_modem_ssr_unregister_notifier(penv);
 
